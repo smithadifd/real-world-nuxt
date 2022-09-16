809c364001ac75049eb33f34487f175ec88313ad
smithadifd <as@retailarchitects.com>
2022-09-16 09:35:26 -0400

    SEO via default meta tags.

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,19 @@
     components: {
       NavBar,
     },
+    head() {
+      return {
+        titleTemplate: '%s - Real World Events',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content:
+              'Where you can find all the events taking place in your neighborhood'
+          }
+        ]
+      }
+    },
   };
 </script>
 <style>
