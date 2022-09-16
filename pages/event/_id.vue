81f23a7042d1bb69445d8ecbf8e37c665ac3b631
smithadifd <as@retailarchitects.com>
2022-09-16 09:54:03 -0400

    Dynamic routes and custom error page.

---
+++ pages/event/_id.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Events #{{ id }}</h1>
+  </div>
+</template>
+<script>
+  export default {
+    head() {
+      return {
+        title: `Event #${this.id}`,
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: `This is the event #${this.id}`,
+          }
+        ]
+      }
+    },
+    computed: {
+      id() {
+        return this.$route.params.id
+      },
+    }
+  }
+</script>
